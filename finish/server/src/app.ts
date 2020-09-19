f858a8d55fa20b3ca6b2532b01348c1cf9c94d55
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2020-09-19 20:43:32 +0700

    setup prettier with eslint to blend each other and implement in app.ts

--- finish/server/src/app.ts
+++ finish/server/src/app.ts
@@ -1,15 +1,15 @@
-import express from "express"
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 
-const app = express()
-const PORT = process.env.PORT || 8080
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.use(cors())
+app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!')
-})
+  res.send('Hello, World!');
+});
 
 app.listen(PORT, () => {
-  console.info(`Example app listening at http://localhost:${PORT}`)
-})
+  console.info(`Example app listening at http://localhost:${PORT}`);
+});
