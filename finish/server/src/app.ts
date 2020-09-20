8715e76982ab2e334b10fb3f7c3f9f4c94e8103e
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2020-09-20 11:08:19 +0700

    create all needed routes verbose in app.ts

--- finish/server/src/app.ts
+++ finish/server/src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 
 const app = express();
@@ -6,8 +6,24 @@ const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
+});
+
+app.get('/todos', (req: Request, res: Response) => {
+  res.status(200).send('This is the endpoint to get all the todos');
+});
+
+app.post('/add-todo', (req: Request, res: Response) => {
+  res.status(201).json({ message: 'Todo added' });
+});
+
+app.put('/edit-todo/:id', (req: Request, res: Response) => {
+  res.status(200).json({ message: 'Todo updated', _id: req.params.id });
+});
+
+app.delete('/delete-todo/:id', (req: Request, res: Response) => {
+  res.status(200).json({ message: 'Todo deleted', _id: req.params.id });
 });
 
 app.listen(PORT, () => {
