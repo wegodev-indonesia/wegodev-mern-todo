70588af5a34307c703313003b5929713ba65e2cd
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2020-09-24 16:24:06 +0700

    add webfontloader, Raleway typeface, first tailwind component

--- finish/client/src/App.tsx
+++ finish/client/src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div>Hello, React with Tailwind!</div>
-  );
+    <div className="lg:container lg:mx-auto">
+      <div className="max-w-sm rounded overflow-hidden shadow-lg m-5">
+        <img className="w-full" src={require('./assets/images/image.jpg')} alt="developer working" />
+        <div className="px-6 py-4">
+          <h1 className="font-bold text-purple-700 text-xl mb-2">Hello</h1>
+          <p className="text-gray-700 text-base">
+            Wkwkwkwk ya kali ah <span role="img" aria-label="emoji">😆</span>
+          </p>
+        </div>
+        <div className="px-6 py-4">
+          <span className="bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
+            #FrontendEngineer #Coder #Hehe
+          </span>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App;
--- finish/client/src/index.tsx
+++ finish/client/src/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/main.css';
+import webFontLoader from 'webfontloader';
+
+import './assets/styles/main.css';
 import App from './App';
+
+webFontLoader.load({
+  google: {
+    families: ["Raleway:400,700:latin"]
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
