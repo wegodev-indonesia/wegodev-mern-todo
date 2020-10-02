5155be42e598bdec1f65aad6065d50e9498591f1
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2020-10-02 17:03:30 +0700

    Create popup Form with react-transition-group

--- finish/client/src/components/Form/index.tsx
+++ finish/client/src/components/Form/index.tsx
@@ -1,15 +1,40 @@
 import React from 'react'
+import classnames from 'classnames'
 import { useForm } from 'react-hook-form'
+import { Transition } from 'react-transition-group'
 import { useMutation, useQueryCache } from 'react-query'
 
 import { postTodo } from 'api/postTodo'
+
+import CloseIcon from 'assets/svg/close'
 
 type Inputs = {
   title: string,
   status: 'completed' | 'uncompleted'
 }
 
-const Form = () => {
+type Props = {
+  inProp: boolean,
+  onClose: () => void
+}
+
+const DURATION = 240
+
+const defaultStyle = {
+  transition: `bottom ${DURATION}ms ease-in-out, opacity ${DURATION * 2}ms ease-in-out`,
+  opacity: 0,
+  bottom: '-180px'
+}
+
+const transitionStyles = {
+  unmounted: { bottom: '-180px', opacity: 0 },
+  entering: { bottom: 0, opacity: 1 },
+  entered:  { bottom: 0, opacity: 1 },
+  exiting:  { bottom: '-180px', opacity: 0 },
+  exited:  { bottom: '-180px', opacity: 0 },
+};
+
+const Form: React.FC<Props> = ({ inProp, onClose }) => {
   const cache = useQueryCache()
 
   const { register, handleSubmit, errors, reset } = useForm<Inputs>()
@@ -29,15 +54,84 @@ const Form = () => {
     }
   }
 
+  const placeholderStyle = classnames([
+      'text-darkPurple',
+      'flex-1',
+      'bg-transparent',
+      'outline-none'
+    ].join(' '), {
+      'placeholder-red-400': errors.title
+  })
+
+  const inputStyle = classnames('flex justify-center items-center bg-gray-200 px-4 py-2 rounded-lg box-border', {
+    'bg-red-200': errors.title
+  })
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="mb-2">
-      <input name="title" placeholder="test" ref={register({ required: true, maxLength: 10 })} />
-      {errors.title && <span className="text-white">This field is required!</span>}
+    <Transition in={inProp} timeout={DURATION}>
+      {(state) => {
+        return (
+          <div
+            style={{
+              ...defaultStyle,
+              ...transitionStyles[state]
+            }}
+            className="fixed flex flex-col z-10 inset-x-0 rounded-t-lg p-4 h-32 bg-white"
+          >
+            <form className={inputStyle} onSubmit={handleSubmit(onSubmit)}>
+              <input
+                name="title"
+                placeholder={errors.title ? '...Oops!' : 'Belajar di wegodev'}
+                className={placeholderStyle}
+                ref={register({
+                  required: { 
+                    value: true,
+                    message: 'This field is required!'
+                  },
+                  maxLength: {
+                    value: 30,
+                    message: 'No more thant 30 characters!'
+                  },
+                  minLength: {
+                    value: 8,
+                    message: 'Minimum characters is 8!'
+                  }
+                })}
+              />
+              <input name="status" defaultValue="uncompleted" ref={register} className="hidden" />
 
-      <input name="status" defaultValue="uncompleted" ref={register} className="hidden" />
+              {errors.title ? (
+                <button
+                onClick={() => reset()}
+                className="bg-transparent text-md font-bold text-darkPurple outline-none ml-1"
+              >
+                Reset
+              </button>
+              ) : (
+                <input
+                  type="submit"
+                  value="Add"
+                  className="bg-transparent text-md font-bold text-darkPurple outline-none ml-1"
+                />
+              )}
+            </form>
+            {errors.title && (
+              <span className="text-xs text-red-500 font-semibold tracking-wide mt-2 pl-1">{errors?.title?.message}</span>
+            )}
 
-      <input type="submit" />
-    </form>
+            <span
+              className="absolute transform -translate-x-1/2 -translate-y-1/2"
+              style={{
+                bottom: '10px',
+                left: '50%'
+              }}>
+              <CloseIcon onClick={onClose} />
+            </span>
+
+          </div>
+        )
+      }}
+    </Transition>
   )
 }
 
